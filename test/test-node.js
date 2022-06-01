c226f4767b2c6235f68281c5a7d9803d20d0b614
0xMoldy <moooldyyy@gmail.com>
2022-06-01 07:55:52 -0700

    Setup

---
+++ test/test-node.js
@@ -0,0 +1,12 @@
+import { expect as want } from 'chai';
+import { Node } from '../node.js';
+
+describe('node', () => {
+  let ali, bob;
+  beforeEach(async () => {
+    ali = new Node([]);
+    bob = new Node([]);
+  });
+
+  it('basic', async () => {});
+});
